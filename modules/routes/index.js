387c039d75be4fac209048eb728c64354603d83a
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-21 15:29:17 -0500

    merged

--- modules/routes/index.js
+++ modules/routes/index.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var path = require( 'path' );
+var bodyParser = require('body-parser');
+
+
+router.use(bodyParser.urlencoded({
+  extend: true
+}));
+router.use(bodyParser.json());
+
 
 router.get('/', function(req, res){
   console.log('Base URL hit');
   res.sendFile( path.resolve( 'public/view/index.html' ) );
 });
+
+router.post('/', function(req, res) {
+  console.log('in base url posst hit', req.body);
+  user.findOne({
+    email: req.body.username
+  }, function(err, email) {
+    if (err) {
+      console.log('find user error: ', err);
+      res.sendStatus(400);
+    } else {
+      if (email != undefined) {
+        res.send('success');
+      } else {
+        console.log('no user found');
+        res.send('no user found');
+      }
+    }
+  });
+});
+
+
+module.exports = router;
+
 
 module.exports = router;
