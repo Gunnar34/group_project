6735db2461c12f62f8980b3dc9eb8fe53aca6202
Ben Zeise <ben.zeise@gmail.com>
2017-07-21 09:34:19 -0500

    just some comments to test git merging

--- public/scripts/services/http.service.js
+++ public/scripts/services/http.service.js
@@ -7,31 +7,35 @@ app.service('httpService', function($http) {
       console.log('service getItem: ', response);
       return response;
     });
-  };
+  }; // end getItem
+
   sv.getWithID = function(path, id) {
-    return $http.get(path + '/'+ id).then(function(response) {
+    return $http.get(path + '/' + id).then(function(response) {
       console.log('service getWithId: ', response);
       return response;
     });
-  };
+  }; // end getWithID
 
   sv.postItem = function(path, its) {
     return $http.post(path, its).then(function(response) {
       console.log('service posted', response);
       return response;
     });
-  };
+  }; // end postItem
+
   sv.putItem = function(path, id, its) {
     console.log('its: ', its);
-      return $http.put(path + '/' + id, its).then(function(response){
-        console.log('service updated', response);
-        return response;
-      });
-    };
+    return $http.put(path + '/' + id, its).then(function(response) {
+      console.log('service updated', response);
+      return response;
+    });
+  }; // end putItem
+
   sv.deleteItem = function(path, id) {
-      return $http.delete(path + '/'+ id).then(function(response){
-        console.log('service deleted', response);
-        return response;
-      });
-    };
-});
+    return $http.delete(path + '/' + id).then(function(response) {
+      console.log('service deleted', response);
+      return response;
+    });
+  }; // end deleteItem
+
+}); // end service
