dcb96b5e1788c90bfb70ec8ea7896972c2a5d381
Noah Rolf <noahrolf1@gmail.com>
2017-07-24 10:42:04 -0500

    asdlnfgljsnfg

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+var authConfigs = {
+  googleAuth: {
+  clientId: '592934163579-7r6khh43nfb3df7utal9m0hn6f3i7cii.apps.googleusercontent.com',
+  clientSecret: 'BoQofZuCmbbwgyCibAAuLgpe',
+  callbackUrl: 'http://localhost:3000/auth/google/callback',
+  },
+  sessionVars: {
+    secret: 'abamath',
+  },
+};
+
+module.exports = authConfigs;
