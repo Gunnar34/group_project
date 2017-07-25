b85fef995cc3a383175ce153fb5b7459ea6f78d1
Noah Rolf <noahrolf1@gmail.com>
2017-07-25 09:11:58 -0500

    added working auth route for adding a user via admin

--- routes/private/classes.js
+++ routes/private/classes.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var user = require('../../services/user');
 
-router.get('/', function (req, res) {
-  res.send({ message: 'nice classes and shit' });
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/', function (req, res) {
+  user.createUser(req.body.email, function(err, user){
+    if (err) {
+      res.send(err);
+    }
+      res.send(user);
+
+  });
 });
 
 module.exports = router;
