58d346b35767086e190636beedf12b67b0d3fa1b
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-26 09:36:38 -0500

    students route

--- routes/private/students.js
+++ routes/private/students.js
@@ -5,4 +5,34 @@ router.get('/', function (req, res) {
   res.send({ message: 'nice students and shit' });
 });
 
+router.put('/:id', function(req, res) {
+    console.log('db add student', req.params.id);
+var myQuery = {
+    _id: req.params.id
+};
+console.log(myQuery);
+console.log(req.body.id);
+var newValues = { $push:
+    { students: {
+      parentID: req.body.parentID,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      grade: req.body.grade,
+      selfCheck: req.body.selfCheck,
+      receiveTexts: req.body.receiveTexts,
+      usePin: req.body.usePin,
+      emergencyInfo: req.body.emergencyInfo
+            }
+          }
+        };
+console.log('new: ', newValues);
+    patients.findOneAndUpdate(myQuery, newValues, function(err) {
+        if (!err) {
+            res.send('nudes');
+        } else {
+            res.send('error');
+        }
+    });
+}); 
+
 module.exports = router;
