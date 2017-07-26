86ed782ca8bc4fae474abd3d080c335e63398719
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-26 16:34:53 -0500

    push students emergencies

--- public/scripts/defineClasses.js
+++ public/scripts/defineClasses.js
@@ -1,16 +1,19 @@
 class Student {
 
-  constructor(parentID, firstName, lastName, grade, emergencyInfo) {
+  constructor(parentID, firstName, lastName, grade, emergencyName, emergencyPhone, emergencyRelation) {
     this.studentId = parentID + '$' + new Date();//change to dat now
     this.firstName = firstName;
     this.lastName = lastName;
     this.grade = grade;
+    this.emergencyName = emergencyName;
+    this.emergencyPhone = emergencyPhone;
+    this.emergencyRelation = emergencyRelation;
     this.selfCheck = false;
     this.receiveTexts = false;
     this.usePin = false;
     this.pin = null;
     this.checkedIn = false;
-    this.emergencyInfo = emergencyInfo;
+
   } // end constructor
 }
 
