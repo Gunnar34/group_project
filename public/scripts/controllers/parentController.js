b57b52fa772237b051b51ec7862554fbb039333e
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-27 12:55:34 -0500

    client route added

--- public/scripts/controllers/parentController.js
+++ public/scripts/controllers/parentController.js
@@ -1,6 +1,7 @@
-app.controller('ParentController', function(dataService, $location) {
+app.controller('ParentController', function(dataService, httpService, $location) {
 
   const vm = this;
+  const hs = httpService;
   vm.pinEntry = '';
   // dataService.currentStudent = dataService.currentStudent;
   // vm.currentStudent = {};
@@ -78,8 +79,13 @@ app.controller('ParentController', function(dataService, $location) {
   }; // end enterPin
 
   vm.completeParentReview = function() {
-    dataService.studentArray[dataService.index] = dataService.currentStudent;
+    // dataService.studentArray[dataService.index] = dataService.currentStudent;
+    id = dataService.currentStudent.studentID;
+    hs.putItem('/students/init', id, dataService.currentStudent).then(function(res){
+
+    });
     vm.go('/complete');
   }; // end completeParentReview
+
 
 }); // end ParentController
