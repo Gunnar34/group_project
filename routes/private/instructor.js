b99c66d6d3b84812585397ed23e0daf93bc55441
Noah Rolf <noahrolf1@gmail.com>
2017-07-31 09:28:35 -0500

    merge master

--- routes/private/instructor.js
+++ routes/private/instructor.js
@@ -5,25 +5,28 @@ var user = require('../../services/user');
 var User = require('../../models/user');
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 
-router.get('/', function(req, res){
-  User.find(function(err, user){
-    if (err) {
-      res.send(err);
-    }
-    res.send(user);
-  });
+router.get('/', function(req, res) {
+	User.find(function(err, user) {
+		if (err) {
+			res.send(err);
+		}
+		res.send(user);
+	});
 });
 
-router.post('/', function (req, res) {
-  user.createUser(req.body.email, function(err, user){
-    if (err) {
-      res.send(err);
-    }
-      res.send(user);
-
-  });//end creat user
-});//end post
+router.post('/', function(req, res) {
+	user.createUser(req.body.email, function(err, user) {
+		if (err) {
+			res.send(err);
+		}
+		res.send(user);
+	}); //end creat user
+}); //end post
 
 module.exports = router;
