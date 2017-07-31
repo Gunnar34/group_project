109a0cbbaee0e95886b137df82a5357cee8e8608
Noah Rolf <noahrolf1@gmail.com>
2017-07-31 09:28:35 -0500

    merge master

--- public/scripts/controllers/instructorController.js
+++ public/scripts/controllers/instructorController.js
@@ -1,47 +1,43 @@
 app.controller('InstructorController', InstuctorController);
 
 function InstuctorController(httpService, AuthFactory, $window, $location) {
-  console.log('in InstuctorController');
-  const vm = this;
-  vm.edit = false;
-  vm.notEdit = true;
+	const vm = this;
+	vm.edit = false;
+	vm.notEdit = true;
 
-  vm.editB = function(){
-    vm.edit = !vm.edit;
-    vm.notEdit = !vm.notEdit;
-  };
+	vm.editB = function() {
+		vm.edit = !vm.edit;
+		vm.notEdit = !vm.notEdit;
+	};
 
-  httpService.getItem('auth').then(function(res){
-    if (res.data.name) {
-      vm.admin = res.data.name.admin;
-      vm.name = res.data.name.googleName;
-    }
-    else {
-      alert('Please Login before viewing this page');
-      $location.path('/');
-    }
-  });
+	httpService.getItem('auth').then(function(res) {
+		if (res.data.name) {
+			vm.admin = res.data.name.admin;
+			vm.name = res.data.name.googleName;
+		} else {
+			alert('Please Login before viewing this page');
+			$location.path('/');
+		}
+	});
 
-  vm.addUser = function(){
-    console.log(vm.email);
-    if (vm.email) {
-      let itemToSend = {
-        email: vm.email
-      };
-      httpService.postItem('private/instructor', itemToSend).then(function(res){
-        vm.email = undefined;
-        vm.getInstructors();
-      });
-    } //end if
-    else {
-      alert('please enter an email before submitting')
-    }
-  };
+	vm.addUser = function() {
+		if (vm.email) {
+			let itemToSend = {
+				email: vm.email
+			};
+			httpService.postItem('private/instructor', itemToSend).then(function() {
+				vm.email = undefined;
+				vm.getInstructors();
+			});
+		} else {
+			//end if
+			alert('please enter an email before submitting');
+		}
+	};
 
-  vm.getInstructors = function(){
-    httpService.getItem('private/instructor').then(function(res){
-      vm.users = res.data;
-    });
-  };
-
-}//end InstuctorController
+	vm.getInstructors = function() {
+		httpService.getItem('private/instructor').then(function(res) {
+			vm.users = res.data;
+		});
+	};
+} //end InstuctorController
--- services/user.js
+++ services/user.js
@@ -7,78 +7,90 @@
  * @function findUserByGoogleId finds a User by their Google id
  * @function create a User that will be authenticated by Google
  */
+/*global require*/
+/*global module*/
+/*eslint no-undef: "error"*/
 var User = require('../models/user');
 
 var UserService = {
-  findUserById: function(id, callback) {
-    User.findById(id, function(err, user) {
-      if (err) {
-        return callback(err, null);
-      }
+	findUserById: function(id, callback) {
+		User.findById(id, function(err, user) {
+			if (err) {
+				return callback(err, null);
+			}
 
-      return callback(null, user);
-    });
-  },
+			return callback(null, user);
+		});
+	},
 
-  findUserByGoogleId: function(id, callback) {
-    User.findOne({
-      googleId: id
-    }, function(err, user) {
+	findUserByGoogleId: function(id, callback) {
+		User.findOne(
+			{
+				googleId: id
+			},
+			function(err, user) {
+				if (err) {
+					return callback(err, null);
+				}
 
-      if (err) {
-        return callback(err, null);
-      }
+				return callback(null, user);
+			}
+		);
+	},
 
-      return callback(null, user);
-    });
-  },
+	createGoogleUser: function(id, token, name, email, callback) {
+		var user = {
+			$set: {
+				googleId: id,
+				googleToken: token,
+				googleName: name
+			}
+		};
+		User.findOneAndUpdate(
+			{
+				googleEmail: email
+			},
+			user,
+			function(err, user) {
+				if (err) {
+					return callback(err, null);
+				}
 
-  createGoogleUser: function(id, token, name, email, callback) {
-    var user = {
-      $set: {
-        googleId: id,
-        googleToken: token,
-        googleName: name
-      }
-    };
-    User.findOneAndUpdate({
-      googleEmail: email
-    }, user, function(err, user) {
-      if (err) {
-        return callback(err, null);
-      }
+				return callback(null, user);
+			}
+		);
+	},
 
-      return callback(null, user);
-    });
-  },
+	findUserByEmail: function(email, callback) {
+		console.log(email);
+		User.findOne(
+			{
+				googleEmail: email
+			},
+			function(err, user) {
+				if (user === null || err) {
+					console.log(user, err);
+					return callback(user, err);
+				}
+				console.log('callback', user);
+				return callback(null, user);
+			}
+		);
+	},
 
-  findUserByEmail: function(email, callback) {
-    console.log(email);
-    User.findOne({
-      googleEmail: email
-    }, function(err, user) {
-      if (user === null || err) {
-        console.log(user, err);
-        return callback(user, err);
-      }
-      console.log('callback', user);
-      return callback(null, user);
-    });
-  },
+	createUser: function(email, callback) {
+		var user = new User();
 
-  createUser: function(email, callback) {
-    var user = new User();
+		user.googleEmail = email;
 
-    user.googleEmail = email;
+		user.save(function(err) {
+			if (err) {
+				return callback(err, null);
+			}
 
-    user.save(function(err) {
-      if (err) {
-        return callback(err, null);
-      }
-
-      return callback(null, user);
-    });
-  }
+			return callback(null, user);
+		});
+	}
 };
 
 module.exports = UserService;
