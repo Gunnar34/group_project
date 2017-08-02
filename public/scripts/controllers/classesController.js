98ebdd17150056cead31fc665b5d67bf418037ef
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-02 08:44:12 -0500

    donut chart

--- public/scripts/controllers/classesController.js
+++ public/scripts/controllers/classesController.js
@@ -1,187 +1,252 @@
 app.controller('ClassesController', function(httpService, $location) {
-	console.log('loaded CC');
+  console.log('loaded CC');
 
-	//make sure its an authorized user
-	httpService.getItem('auth').then(function(res) {
-		if (res.data.name) {
-			console.log(res.data.name);
-			vm.admin = res.data.name.admin;
-			vm.name = res.data.name.googleName;
-			vm.userId = res.data.name._id;
-			if (!res.data.name.phone) {
-				setTimeout(function() {
-					document.getElementById('addPhoneNumber').style.display = 'block';
-				}, 600);
-			}
-		} else {
-			swal({
-				title: 'Oops!',
-				text: "Please login to continue",
-				imageUrl: 'public/assets/images/abamath.png',
-				imageWidth: 150,
-				imageHeight: 150,
-				animation: false
-			});
-			$location.path('/');
-		}
-	});
+  //make sure its an authorized user
+  httpService.getItem('auth').then(function(res) {
+    if (res.data.name) {
+      console.log(res.data.name);
+      vm.admin = res.data.name.admin;
+      vm.name = res.data.name.googleName;
+      vm.userId = res.data.name._id;
+      if (!res.data.name.phone) {
+        setTimeout(function() {
+          document.getElementById('addPhoneNumber').style.display = 'block';
+        }, 600);
+      }
+    } else {
+      swal({
+        title: 'Oops!',
+        text: "Please login to continue",
+        imageUrl: 'public/assets/images/abamath.png',
+        imageWidth: 150,
+        imageHeight: 150,
+        animation: false
+      });
+      $location.path('/');
+    }
+  });
 
-	//vars
-	var vm = this;
-	vm.inputNumber = [0];
-	var number = 1;
-	vm.instructorsUP;
-	localStorage.setItem('classView', false);
-	localStorage.setItem('notParentView', true);
-	vm.gradesRange = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //vars
+  var vm = this;
+  var canvas = document.getElementById("myChart");
+  var ctx = canvas.getContext('2d');
+  Chart.defaults.global.defaultFontSize = 200;
+  vm.inputNumber = [0];
+  var number = 1;
+  vm.instructorsUP;
+  localStorage.setItem('classView', false);
+  localStorage.setItem('notParentView', true);
+  vm.gradesRange = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-	var showToast = function(message, duration){
-		Materialize.toast(message, duration);
-	};//end showToast
+  var dataArr = [];
+  var labelArr = [];
 
-	vm.addInput = function() {
-		vm.inputNumber.push(number);
-		number++;
-	}; //end addInput //allows adding instructor inputs
+  data = {
+    labels: labelArr,
+    pointLabelFontSize: 100,
+    scaleFontSize: 100,
+    datasets: [{
+      data: dataArr,
+      backgroundColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        '#bad455',
+				'rgb(201, 40, 196)',
+				'rgb(247, 4, 4)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        '#bad455'
 
-	vm.subInput = function() {
-		if (vm.inputNumber.length > 1) {
-			vm.inputNumber.pop();
-		}
-	}; //end subInput //allows removing an instructor input
+      ],
+      hoverBorderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        '#bad455'
+      ]
+    }],
 
-	window.onclick = function(event) {
-		let id = event.target.getAttribute('id');
-		if (event.target.getAttribute('class') == 'modal') {
-			document.getElementById(id).style.display = 'none';
-		}
-	}; //end window.onclick //allows clicking outside the modal to close
+    // These labels appear in the legend and in the tooltips when hovering different arcs
 
-	vm.savePhone = function() {
-		let its = {
-			phone: vm.phone
-		}
-		httpService.putItem('/private/instructor', vm.userId, its).then(function(res) {
-			console.log(res);
-			document.getElementById('addPhoneNumber').style.display = 'none';
-			showToast('Saved', 1500);
-		});
-	};
+  };
 
-	vm.editClass = function(index) {
-		vm.id = vm.classesArray[index]._id;
-		vm.gradesUP = vm.classesArray[index].grades;
-		vm.subjectUP = vm.classesArray[index].subject;
-		vm.startDateUP = vm.classesArray[index].startDate;
-		vm.endDateUP = vm.classesArray[index].endDate;
-		vm.startTimeUP = vm.classesArray[index].startTime;
-		vm.endTimeUP = vm.classesArray[index].startTime;
-		vm.locationUP = vm.classesArray[index].location;
-		vm.instructorsUP = vm.classesArray[index].instructors;
-		document.getElementById('editClass').style.display = 'block';
-	};
+  options = {};
 
-	vm.addEditInput = function() {
-		vm.instructorsUP.push({
-			instructor: ''
-		});
-	}; //end addEditInput
 
-	vm.subEditInput = function() {
-		if (vm.instructorsUP.length > 1) {
-			vm.instructorsUP.pop();
-		}
-	}; //end subEditInput
 
-	vm.saveEdit = function() {
-		let itemToSend = {
-			grades: vm.gradesUP,
-			subject: vm.subjectUP,
-			startDate: vm.startDateUP,
-			endDate: vm.endDateUP,
-			startTime: vm.startTimeUP,
-			endTime: vm.endTimeUP,
-			location: vm.locationUP,
-			instructors: vm.instructorsUP
-		};
-		httpService.putItem('/private/classes/classes', vm.id, itemToSend).then(function() {
-			vm.populateClasses();
-			document.getElementById('editClass').style.display = 'none';
-			showToast('Edit Saved', 1500);
-		});
-	}; //end saveEdit
 
-	vm.addClass = function() {
+  var showToast = function(message, duration) {
+    Materialize.toast(message, duration);
+  }; //end showToast
 
-		let instructorsArray = [];
-		for (var i = 0; i < vm.inputNumber.length; i++) {
-			let instructorName = vm.instructor[i];
-			instructorsArray.push({
-				instructor: instructorName
-			});
-		}
-		let objectToSend = {
-			grades: vm.gradesArray,
-			location: vm.location,
-			subject: vm.subject,
-			startDate: vm.startDate,
-			endDate: vm.endDate,
-			startTime: vm.startTime,
-			endTime: vm.endTime,
-			instructors: instructorsArray,
-			students: []
-		};
-		console.log(objectToSend);
-		httpService.postItem('private/classes/classes', objectToSend).then(function(res) {
-			console.log(res);
-			vm.populateClasses(); //repopulate classes in table
-		}); //end then function
-		document.getElementById('addClass').style.display = 'none'; //close modal
-		document.getElementById('addClassForm').reset();
-		vm.inputNumber = [0];
-	}; //end addClass
+  vm.addInput = function() {
+    vm.inputNumber.push(number);
+    number++;
+  }; //end addInput //allows adding instructor inputs
 
-	vm.populateClasses = function() {
-		console.log('in populateClasses');
-		httpService.getItem('private/classes/classes').then(function(res) {
-			vm.classesArray = res.data[0];
-		}); //end http get popClasses
-	}; //end populateClasses
+  vm.subInput = function() {
+    if (vm.inputNumber.length > 1) {
+      vm.inputNumber.pop();
+    }
+  }; //end subInput //allows removing an instructor input
 
-	vm.removeClass = function(id) {
-		swal({
-			title: 'Are you sure you want to delete this class?',
-			text: "You won't be able to undo this!",
-			imageUrl: 'public/assets/images/abamath.png',
-			imageWidth: 150,
-			imageHeight: 150,
-			animation: true,
-			showCancelButton: true,
-			confirmButtonColor: '#2196f3',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes, delete it!'
-		}).then(function() {
-			httpService.deleteItem('/private/classes', id).then(function(res) {
-				console.log('deleted', res);
-				vm.populateClasses();
-			}); //end deleteItem
-			// swal({
-			// 	title: 'Deleted!',
-			// 	text: "The class was deleted",
-			// 	imageUrl: 'public/assets/images/abamath.png',
-			// 	imageWidth: 150,
-			// 	imageHeight: 150,
-			// 	animation: false
-			// });
-			showToast('Class Deleted', 2000);
-		});
-	}; //end remove class
+  window.onclick = function(event) {
+    let id = event.target.getAttribute('id');
+    if (event.target.getAttribute('class') == 'modal') {
+      document.getElementById(id).style.display = 'none';
+    }
+  }; //end window.onclick //allows clicking outside the modal to close
 
-	vm.classView = function(x) {
-		console.log(x); //takes the class Id that was clicked and stores it
-		localStorage.setItem('classID', x);
-		$location.path('/students');
-		localStorage.setItem('classView', true);
-	}; //end classView
+  vm.savePhone = function() {
+    let its = {
+      phone: vm.phone
+    }
+    httpService.putItem('/private/instructor', vm.userId, its).then(function(res) {
+      console.log(res);
+      document.getElementById('addPhoneNumber').style.display = 'none';
+      showToast('Saved', 1500);
+    });
+  };
+
+  vm.editClass = function(index) {
+    vm.id = vm.classesArray[index]._id;
+    vm.gradesUP = vm.classesArray[index].grades;
+    vm.subjectUP = vm.classesArray[index].subject;
+    vm.startDateUP = vm.classesArray[index].startDate;
+    vm.endDateUP = vm.classesArray[index].endDate;
+    vm.startTimeUP = vm.classesArray[index].startTime;
+    vm.endTimeUP = vm.classesArray[index].startTime;
+    vm.locationUP = vm.classesArray[index].location;
+    vm.instructorsUP = vm.classesArray[index].instructors;
+    document.getElementById('editClass').style.display = 'block';
+  };
+
+  vm.addEditInput = function() {
+    vm.instructorsUP.push({
+      instructor: ''
+    });
+  }; //end addEditInput
+
+  vm.subEditInput = function() {
+    if (vm.instructorsUP.length > 1) {
+      vm.instructorsUP.pop();
+    }
+  }; //end subEditInput
+
+  vm.saveEdit = function() {
+    let itemToSend = {
+      grades: vm.gradesUP,
+      subject: vm.subjectUP,
+      startDate: vm.startDateUP,
+      endDate: vm.endDateUP,
+      startTime: vm.startTimeUP,
+      endTime: vm.endTimeUP,
+      location: vm.locationUP,
+      instructors: vm.instructorsUP
+    };
+    httpService.putItem('/private/classes/classes', vm.id, itemToSend).then(function() {
+      vm.populateClasses();
+      document.getElementById('editClass').style.display = 'none';
+      showToast('Edit Saved', 1500);
+    });
+  }; //end saveEdit
+
+  vm.addClass = function() {
+
+    let instructorsArray = [];
+    for (var i = 0; i < vm.inputNumber.length; i++) {
+      let instructorName = vm.instructor[i];
+      instructorsArray.push({
+        instructor: instructorName
+      });
+    }
+    let objectToSend = {
+      grades: vm.gradesArray,
+      location: vm.location,
+      subject: vm.subject,
+      startDate: vm.startDate,
+      endDate: vm.endDate,
+      startTime: vm.startTime,
+      endTime: vm.endTime,
+      instructors: instructorsArray,
+      students: []
+    };
+    console.log(objectToSend);
+    httpService.postItem('private/classes/classes', objectToSend).then(function(res) {
+      console.log(res);
+      vm.populateClasses(); //repopulate classes in table
+    }); //end then function
+    document.getElementById('addClass').style.display = 'none'; //close modal
+    document.getElementById('addClassForm').reset();
+    vm.inputNumber = [0];
+  }; //end addClass
+
+  vm.populateClasses = function() {
+    console.log('in populateClasses');
+    httpService.getItem('private/classes/classes').then(function(res) {
+      vm.classesArray = res.data[0];
+
+      for (var i = 0; i < vm.classesArray.length; i++) {
+        labelArr.push(vm.classesArray[i].location);
+        dataArr.push(vm.classesArray[i].students.length);
+      }
+      var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: options
+      });
+    });
+
+    //end http get popClasses
+  }; //end populateClasses
+
+  vm.removeClass = function(id) {
+    swal({
+      title: 'Are you sure you want to delete this class?',
+      text: "You won't be able to undo this!",
+      imageUrl: 'public/assets/images/abamath.png',
+      imageWidth: 150,
+      imageHeight: 150,
+      animation: true,
+      showCancelButton: true,
+      confirmButtonColor: '#2196f3',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(function() {
+      httpService.deleteItem('/private/classes', id).then(function(res) {
+        console.log('deleted', res);
+        vm.populateClasses();
+      }); //end deleteItem
+      // swal({
+      // 	title: 'Deleted!',
+      // 	text: "The class was deleted",
+      // 	imageUrl: 'public/assets/images/abamath.png',
+      // 	imageWidth: 150,
+      // 	imageHeight: 150,
+      // 	animation: false
+      // });
+      showToast('Class Deleted', 2000);
+    });
+  }; //end remove class
+
+  vm.classView = function(x) {
+    console.log(x); //takes the class Id that was clicked and stores it
+    localStorage.setItem('classID', x);
+    $location.path('/students');
+    localStorage.setItem('classView', true);
+  }; //end classView
 
 });
