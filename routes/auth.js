3e1fe7a3222d7896d3e3b6326a79a0bd2292422c
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-03 20:29:05 -0500

    time tracking

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('../auth/passport');
+var logData = require('../models/log.js');
 /**
  * GET /auth/google
  *
@@ -15,8 +16,8 @@ var passport = require('../auth/passport');
  * to select a Google account.
  */
 router.get('/google', passport.authenticate('google', {
-	scope: ['openid', 'email'],
-	prompt: 'select_account',
+  scope: ['openid', 'email'],
+  prompt: 'select_account',
 }));
 /**
  * GET /auth/google/callback
@@ -28,8 +29,8 @@ router.get('/google', passport.authenticate('google', {
  * callbackUrl in {@link config/auth}.
  */
 router.get('/google/callback', passport.authenticate('google', {
-	successRedirect: '/#!/classes', // take them to their private data
-	failureRedirect: '/#!/failure' // take them to failure to try again})
+  successRedirect: '/#!/classes', // take them to their private data
+  failureRedirect: '/#!/failure' // take them to failure to try again})
 }));
 /**
  * GET /auth
@@ -39,16 +40,16 @@ router.get('/google/callback', passport.authenticate('google', {
  * @return JSON object with status (true or false) and, if true, user's name
  */
 router.get('/', function(req, res) {
-	if (req.isAuthenticated()) {
-		res.json({
-			status: true,
-			name: req.user
-		});
-	} else {
-		res.json({
-			status: false
-		});
-	}
+  if (req.isAuthenticated()) {
+    res.json({
+      status: true,
+      name: req.user
+    });
+  } else {
+    res.json({
+      status: false
+    });
+  }
 
 });
 /**
@@ -59,8 +60,21 @@ router.get('/', function(req, res) {
  * @return 200 - OK
  */
 router.get('/logout', function(req, res) {
-	req.logout();
-	res.sendStatus(200); // they made it!
+  objectToSave = {
+    user: req.user.googleName,
+    inOrOut: 'OUT',
+    date: new Date()
+  };
+
+	logout = new logData(objectToSave);
+	console.log('logout ', logout);
+  logout.save(function(err) {
+    console.log(err);
+    if (err) return handleError(err);
+		});
+  req.logout();
+  res.sendStatus(200); // they made it!
+
 });
 
 module.exports = router;
