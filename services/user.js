ed3f302d14325f0a44ddeb5a7b3e00b23b2679c7
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-18 12:32:45 -0500

    passpot fixed attempt

--- services/user.js
+++ services/user.js
@@ -13,85 +13,129 @@
 var User = require('../models/user');
 
 var UserService = {
-	findUserById: function(id, callback) {
-		User.findById(id, function(err, user) {
-			if (err) {
-				return callback(err, null);
-			}
+  findUserById: function (id, callback) {
+    User.findById(id, function (err, user) {
+      if (err) {
+        return callback(err, null);
+      }
 
-			return callback(null, user);
-		});
-	},
+      return callback(null, user);
+    });
+  },
 
-	findUserByGoogleId: function(id, callback) {
-		User.findOne(
-			{
-				googleId: id
-			},
-			function(err, user) {
-				if (err) {
-					return callback(err, null);
-				}
+  findUserByGoogleId: function (id, callback) {
+    User.findOne({ googleId: id }, function (err, user) {
 
-				return callback(null, user);
-			}
-		);
-	},
+      if (err) {
+        return callback(err, null);
+      }
 
-	createGoogleUser: function(id, token, name, email, callback) {
-		var user = {
-			$set: {
-				googleId: id,
-				googleToken: token,
-				googleName: name
-			}
-		};
-		User.findOneAndUpdate(
-			{
-				googleEmail: email
-			},
-			user,
-			function(err, user) {
-				if (err) {
-					return callback(err, null);
-				}
+      return callback(null, user);
+    });
+  },
 
-				return callback(null, user);
-			}
-		);
-	},
+  createGoogleUser: function (id, token, name, email, callback) {
+    var user = new User();
 
-	findUserByEmail: function(email, callback) {
-		console.log(email);
-		User.findOne(
-			{
-				googleEmail: email
-			},
-			function(err, user) {
-				if (user === null || err) {
-					console.log(user, err);
-					return callback(user, err);
-				}
-				console.log('callback', user);
-				return callback(null, user);
-			}
-		);
-	},
+    user.googleId = id;
+    user.googleToken = token;
+    user.googleName = name;
+    user.googleEmail = email;
 
-	createUser: function(req, callback) {
-		var user = new User();
+    user.save(function (err) {
+      if (err) {
+        return callback(err, null);
+      }
 
-		user.googleEmail = req.email;
-		user.googleName = req.name;
-
-		user.save(function(err) {
-			if (err) {
-				return callback(err, null);
-			}
-
-			return callback(null, user);
-		});
-	}
+      return callback(null, user);
+    });
+  },
 };
 
 module.exports = UserService;
+
+//
+//
+// var UserService = {
+// 	findUserById: function(id, callback) {
+// 		User.findById(id, function(err, user) {
+// 			if (err) {
+// 				return callback(err, null);
+// 			}
+//
+// 			return callback(null, user);
+// 		});
+// 	},
+//
+// 	findUserByGoogleId: function(id, callback) {
+// 		User.findOne(
+// 			{
+// 				googleId: id
+// 			},
+// 			function(err, user) {
+// 				if (err) {
+// 					return callback(err, null);
+// 				}
+//
+// 				return callback(null, user);
+// 			}
+// 		);
+// 	},
+//
+// 	createGoogleUser: function(id, token, name, email, callback) {
+// 		var user = {
+// 			$set: {
+// 				googleId: id,
+// 				googleToken: token,
+// 				googleName: name
+// 			}
+// 		};
+// 		User.findOneAndUpdate(
+// 			{
+// 				googleEmail: email
+// 			},
+// 			user,
+// 			function(err, user) {
+// 				if (err) {
+// 					return callback(err, null);
+// 				}
+//
+// 				return callback(null, user);
+// 			}
+// 		);
+// 	},
+//
+// 	// findUserByEmail: function(email, callback) {
+// 	// 	console.log(email);
+// 	// 	User.findOne(
+// 	// 		{
+// 	// 			googleEmail: email
+// 	// 		},
+// 	// 		function(err, user) {
+// 	// 			if (user === null || err) {
+// 	// 				console.log(user, err);
+// 	// 				return callback(user, err);
+// 	// 			}
+// 	// 			console.log('callback', user);
+// 	// 			return callback(null, user);
+// 	// 		}
+// 	// 	);
+// 	// },
+//
+// 	createUser: function(req, callback) {
+// 		var user = new User();
+//
+// 		user.googleEmail = req.email;
+// 		user.googleName = req.name;
+//
+// 		user.save(function(err) {
+// 			if (err) {
+// 				return callback(err, null);
+// 			}
+//
+// 			return callback(null, user);
+// 		});
+// 	}
+// };
+//
+// module.exports = UserService;
